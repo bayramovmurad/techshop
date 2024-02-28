e4a07dcd9704dd4285812b474526abd5ded981ac
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-02-28 11:56:33 +0400

    create custom product data

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
-import express from 'express'
-
+import express from 'express';
+import products from './data/Products.js';
+import dotenv  from 'dotenv';
+dotenv.config();
+const port = process.env.PORT || 5678;
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send("hi whats up");
+app.get('/api/products', (req, res) => {
+    res.json(products);
 });
 
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+});
 
-app.listen(5678, () => {
-    console.log('Server is running on port 5678');
-})
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
