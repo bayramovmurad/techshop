a333576d606214a3727835e23e5a86e14387e499
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-03-02 11:29:56 +0400

    edit in serverjs

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
+dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cors from 'cors'
 
-dotenv.config();
 const port = process.env.PORT || 5678;
-connectDB();
 
+connectDB();
 const app = express();
 app.use(cors());
-app.use(notFound);
-app.use(errorHandler);
-
 app.use('/api/products', productRoutes);
-
-
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () =>
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
