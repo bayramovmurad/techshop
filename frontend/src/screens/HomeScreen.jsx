2d76dc3c8d64fe172323cab1496f8a2176d0b110
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-03-02 12:58:35 +0400

    I integrated the redux codes into the interface.

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,29 +1,28 @@
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product/Product';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useGetProductsQuery } from '../redux/slices/productSlice'
+
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('http://localhost:5678/api/products');
-            setProducts(data);
-        };
-
-        fetchProducts();
-    }, []);
+    const { data: products, isLoading, error } = useGetProductsQuery();
     return (
         <>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+            {isLoading ? (
+                <div>Loading...</div>
+            ) : error ? (
+                <div>{error?.data.message || error.error}</div>
+            ) : (
+                <>
+                    <h1>Latest Products</h1>
+                    <Row>
+                        {products.map((product) => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                </>
+            )}
         </>
     );
 };
