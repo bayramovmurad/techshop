8dd05ee4050ed6b26ed0ea923270f2fb9f519ec1
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-03-04 13:23:44 +0400

    edit product in controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { getProducts, getProductsId, createProduct } from '../controllers/productsControllers.js'
+import { getProducts, getProductsId, createProduct, updateProduct } from '../controllers/productsControllers.js'
 import { admin, protect } from '../middleware/authMiddleware.js';
 
-router.get('/', getProducts).post(protect,admin,createProduct);
+router.get('/', getProducts).post(protect, admin, createProduct);
 router.get('/:id', getProductsId);
+router.get('/:id').get(getProductsId).put(protect, admin, updateProduct);
 
 export default router;
