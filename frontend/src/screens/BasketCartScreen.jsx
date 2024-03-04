c89304cdd3585c5abcc29caf94147e914a753c60
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-03-04 08:42:29 +0400

    create shipping screen and private routes

--- frontend/src/screens/BasketCartScreen.jsx
+++ frontend/src/screens/BasketCartScreen.jsx
@@ -31,45 +31,45 @@ const BasketCartScreen = () => {
         <Row>
             <Col md={8}>
                 <h1 style={{ marginBottom: '20px' }}>Shopping Cart</h1>
-                    <ListGroup variant='flush'>
-                        {cartItems.map((item) => (
-                            <ListGroup.Item key={item._id}>
-                                <Row>
-                                    <Col md={2}>
-                                        <Image src={item.image} alt={item.name} fluid rounded />
-                                    </Col>
-                                    <Col md={3}>
-                                        <Link to={`/product/${item._id}`}>{item.name}</Link>
-                                    </Col>
-                                    <Col md={2}>${item.price}</Col>
-                                    <Col md={2}>
-                                        <Form.Control
-                                            as='select'
-                                            value={item.qty}
-                                            onChange={(e) =>
-                                                addToCartHandler(item, Number(e.target.value))
-                                            }
-                                        >
-                                            {[...Array(item.countInStock).keys()].map((x) => (
-                                                <option key={x + 1} value={x + 1}>
-                                                    {x + 1}
-                                                </option>
-                                            ))}
-                                        </Form.Control>
-                                    </Col>
-                                    <Col md={2}>
-                                        <Button
-                                            type='button'
-                                            variant='light'
-                                            onClick={() => removeFromCartHandler(item._id)}
-                                        >
-                                            <FaTrash />
-                                        </Button>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                        ))}
-                    </ListGroup>
+                <ListGroup variant='flush'>
+                    {cartItems.map((item) => (
+                        <ListGroup.Item key={item._id}>
+                            <Row>
+                                <Col md={2}>
+                                    <Image src={item.image} alt={item.name} fluid rounded />
+                                </Col>
+                                <Col md={3}>
+                                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                                </Col>
+                                <Col md={2}>${item.price}</Col>
+                                <Col md={2}>
+                                    <Form.Control
+                                        as='select'
+                                        value={item.qty}
+                                        onChange={(e) =>
+                                            addToCartHandler(item, Number(e.target.value))
+                                        }
+                                    >
+                                        {[...Array(item.countInStock).keys()].map((x) => (
+                                            <option key={x + 1} value={x + 1}>
+                                                {x + 1}
+                                            </option>
+                                        ))}
+                                    </Form.Control>
+                                </Col>
+                                <Col md={2}>
+                                    <Button
+                                        type='button'
+                                        variant='light'
+                                        onClick={() => removeFromCartHandler(item._id)}
+                                    >
+                                        <FaTrash />
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
             </Col>
             <Col md={4}>
                 <Card>
@@ -85,13 +85,17 @@ const BasketCartScreen = () => {
                                 .toFixed(2)}
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Button
-                                type='button'
-                                className='btn-block'
-                                disabled={cartItems.length === 0}
-                            >
-                                Proceed To Checkout
-                            </Button>
+                            <Link to='/shipping'>
+                                <Button
+                                    type='button'
+                                    className='btn-block'
+                                    disabled={cartItems.length === 0}
+                                >
+
+                                    Proceed To Checkout
+
+                                </Button>
+                            </Link>
                         </ListGroup.Item>
                     </ListGroup>
                 </Card>
---
+++ frontend/src/screens/ShippingScreen.jsx
@@ -0,0 +1,8 @@
+const ShippingScreen = () => {
+  return (
+    <div>
+        <h1>Shipping Screen</h1>
+    </div>
+  )
+}
+export default ShippingScreen
