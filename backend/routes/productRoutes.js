d1c713297e13aa7070c74d5d05bc1b23579f6c03
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-03-05 15:27:30 +0400

    update routes in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 import { getProducts, getProductsId, createProduct, updateProduct } from '../controllers/productsControllers.js'
 import { admin, protect } from '../middleware/authMiddleware.js';
 
-router.get('/', getProducts).post(protect, admin, createProduct);
-router.get('/:id', getProductsId);
-router.get('/:id').get(getProductsId).put(protect, admin, updateProduct);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id').get(getProductsId);
+router.route('/:id').get(getProductsId).put(protect, admin, updateProduct);
 
 export default router;
